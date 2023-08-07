2502f8b29d5a6ec312749aabf24d390d342088cc
Mohsinalee <mohsinalee91@gmail.com>
2023-08-08 02:00:35 +0500

    add missing annotation

--- src/main/java/com/systems/ticketsmanagement/repository/ReportRepository.java
+++ src/main/java/com/systems/ticketsmanagement/repository/ReportRepository.java
@@ -2,6 +2,8 @@ package com.systems.ticketsmanagement.repository;
 
 import com.systems.ticketsmanagement.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 }
--- src/main/java/com/systems/ticketsmanagement/repository/TicketRepository.java
+++ src/main/java/com/systems/ticketsmanagement/repository/TicketRepository.java
@@ -2,6 +2,8 @@ package com.systems.ticketsmanagement.repository;
 
 import com.systems.ticketsmanagement.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
